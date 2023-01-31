80df592d7d8a0a49e290a9b8271189b9486fb2e6
SneakyJ <k.nikolaienko@gmail.com>
2023-01-31 21:18:43 +0100

    new calendar component was added with template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './components/content/content.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    CalendarComponent
   ],
   imports: [
     AppRoutingModule,
